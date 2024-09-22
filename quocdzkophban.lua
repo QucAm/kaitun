724935f3b771ea9ffdf19f26a57082113a280389
QucAm <tranquocanh9022@gmail.com>
2024-09-22 21:33:46 +0700

    Create quocdzkophban.lua

---
+++ quocdzkophban.lua
@@ -0,0 +1,61 @@
+_G.Setting = {
+    ['FPS Boost'] = true,
+    ['White Screen'] = false
+}
+_G.Fruit = { 
+    ['Main Fruit'] = {'Dough-Dough'},
+    ['Select Fruit'] = {'Flame-Flame','Ice-Ice','Sand-Sand','Dark-Dark','Light-Light','Magma-Magma','Quake-Quake','Spider-Spider'}
+}
+_G.Race = { 
+    ['Select Race'] = {'Human'},
+    ['Lock Race'] = true,
+    ['Quest Race V3'] = true
+}
+_G.Melee = { 
+    ['Superhuman'] = true,
+    ['Death Step'] = true,
+    ['Sharkman Karate'] = true,
+    ['Electric Claw'] = true,
+    ['Dragon Talon'] = true,
+    ['Godhuman'] = true
+}
+_G.Sword = { 
+    ['Saber'] = true,
+    ['Midnight Blade'] = true,
+    ['Shark Anchor'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Koko'] = false,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['Buddy Sword'] = true,
+    ['Twin Hooks'] = true,
+    ['SpikeyTrident'] = true,
+    ['Hallow Scryte'] = true,
+    ['Dark Dagger'] = true,
+    ['Yama'] = true,
+    ['Tushita'] = true,
+    ['True Triple Katana'] = true,
+    ['Cursed Dual Katana'] = true
+}
+_G.Gun = {
+    ['Kabucha'] = true,
+    ['Acidum Rifle'] = true,
+    ['Soul Guitar'] = true, 
+    ['Serpent Bow'] = true
+}
+_G.Quest = { 
+    ['Quest Aura Haki'] = true,
+    ['Quest Race V4'] = true,
+    ['Quest Dough Awaken'] = true,
+    ['Quest Kill Boss'] = true
+}
+_G.Mastery = { 
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['Fruit'] = true,
+    ['Gun'] = true
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0x739/lua/main/Kaitun.lua"))()
